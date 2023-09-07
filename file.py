8b40aa5c34fdcbeb306599a3da7f4b8de7c49c06
sarthakg77 <142787653+sarthakg77@users.noreply.github.com>
2023-09-07 12:45:51 +0530

    Update file.py

--- file.py
+++ file.py
@@ -4,13 +4,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import openai
 
-# Initialize OpenAI with your API key
-OPENAI_API_KEY = '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'  # Replace with your API key and remember to keep it secure!
-openai.api_key = OPENAI_API_KEY
-
 st.title("Detailed Data Analysis")
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+# Move the OpenAI API key input to the sidebar
+OPENAI_API_KEY = st.sidebar.text_input(label=":key: OpenAI Key:", 
+                                       help="Please ensure you have an OpenAI API account with credit.",
+                                       type="password")
+
+# Check if the key is provided and set it for openai
+if OPENAI_API_KEY:
+    openai.api_key = OPENAI_API_KEY
 
 def get_gpt3_summary(prompt):
     """
@@ -27,75 +30,71 @@ def get_gpt3_summary(prompt):
         st.write("Error with OpenAI API call:", str(e))
         return None
 
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     
-    # Convert 'Date' to datetime and extract month
-    data['Date'] = pd.to_datetime(data['Date'])
-    data['Month'] = data['Date'].dt.month
-
-    # 1. Customer Behavior:
-    payment_distribution = data['Payment'].value_counts(normalize=True) * 100
-    most_preferred_payment = payment_distribution.idxmax()
-    avg_spend_per_transaction = data['Total'].mean()
-
-    # 2. Sales Analysis:
-    product_sales = data.groupby('Product line')['Total'].sum()
-    highest_selling_product = product_sales.idxmax()
-    lowest_selling_product = product_sales.idxmin()
-    total_revenue = data['Total'].sum()
-
-    # 3. Churn Analysis:
-    customer_type_distribution = data['Customer type'].value_counts(normalize=True) * 100
-    member_percentage = customer_type_distribution['Member']
-    avg_products_per_transaction = data['Quantity'].mean()
-
-    # Create a summary for GPT-3
-    summary_prompt = f"""
-    Provide a summary based on the following information:
-    The majority of customers prefer {most_preferred_payment} for transactions, accounting for approximately {payment_distribution[most_preferred_payment]:.2f}% of all transactions. 
-    On average, customers spend ${avg_spend_per_transaction:.2f} per transaction. The top-selling product is {highest_selling_product}, 
-    while {lowest_selling_product} has the least sales. The total revenue is ${total_revenue:,.2f}. 
-    {member_percentage:.2f}% of customers are members. Provide insights and recommendations based on this data.
-    """
-
-    # Get summary from GPT-3
-    gpt3_summary = get_gpt3_summary(summary_prompt)
-
-    # Displaying the GPT-3 generated summary
-    st.write(gpt3_summary)
-
-    # Customer Behavior Analysis Visualizations
-    st.subheader("Customer Behavior Analysis")
-
+    # Displaying Customer Analysis Visualizations
+    st.subheader("Customer Analysis")
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6))
-    sns.countplot(data=data, x='Customer type', ax=ax1)
-    ax1.set_title('Distribution by Customer Type')
-
-    sns.countplot(data=data, x='Gender', ax=ax2)
-    ax2.set_title('Distribution by Gender')
+    sns.countplot(data=data, x='gender', ax=ax1)
+    ax1.set_title('Distribution by Gender')
+    sns.countplot(data=data, x='country', ax=ax2)
+    ax2.set_title('Distribution by Country')
     st.pyplot(fig)
 
     fig, ax = plt.subplots(figsize=(10, 6))
-    sns.countplot(data=data, x='Payment', ax=ax, order=data['Payment'].value_counts().index)
-    ax.set_title('Distribution by Payment Method')
+    sns.countplot(data=data, x='churn', ax=ax)
+    ax.set_title('Churn Distribution (1: Churned, 0: Retained)')
     st.pyplot(fig)
 
-    # Sales Analysis Visualizations
+    # Sales Analysis Visualization
     st.subheader("Sales Analysis")
-
-    # Ensure the 'Total' column is of numeric type
-    data['Total'] = pd.to_numeric(data['Total'], errors='coerce')
-    sales_data_branch = data.groupby('Branch')['Total'].sum().reset_index()
-    sales_data_city = data.groupby('City')['Total'].sum().reset_index()
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6))
-    sns.barplot(data=sales_data_branch, x='Branch', y='Total', errorbar=None, ax=ax1)
-    ax1.set_title('Total Sales per Branch')
-
-    sns.barplot(data=sales_data_city, x='City', y='Total', errorbar=None, ax=ax2)
-    ax2.set_title('Total Sales per City')
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.boxplot(data=data, x='country', y='balance', ax=ax)
+    ax.set_title('Balance Distribution by Country')
     st.pyplot(fig)
+    
+    # Holistic Analysis Section
+    st.subheader("Holistic Analysis")
+    if st.button("Generate Holistic Analysis"):
+        holistic_analysis_prompt = f"""
+        Given the bank's customer data, provide a holistic analysis in bullet points:
 
+        1. **Patterns and Trends**:
+            - Identify patterns related to customer aging based on balance and credit average.
+            - Discover any relationships between credit score, balance, and customer demographics.
+            - Examine if a specific country or age group has a distinct pattern in terms of balance or credit score.
+
+        2. **Optimal Balance and Credit Score**:
+            - Determine the optimal balance with respect to the average credit score.
+            - Consider a specific scenario where the customer is 85 years old with a credit score of 513.
+            - Analyze how this might differ if we only consider customers from Germany.
+
+        3. **Sample Size and Distribution**:
+            - Evaluate the ideal sample size given that Germany accounts for 56% of the instances.
+            - Discuss the significance of having 8014 customers distributed across 12 countries.
+
+        4. **Regression and Correlation Insights**:
+            - Elaborate on the potential reasons for customer churn, and if there's any correlation with other variables.
+            - Examine correlations present in the data, especially with respect to customers from different countries.
+
+        5. **Dimensionality and Data Points**:
+            - Discuss the importance of dimensionality in the dataset.
+            - Highlight key data points that provide the most valuable insights.
+
+        6. **Customer Management and Integration**:
+            - Describe the components of an integrated customer management system.
+            - Discuss how such an integration can enhance customer support and maximize financial worth.
+
+        7. **Data Models and Storage**:
+            - Evaluate the efficiency of the data models in place.
+            - Discuss the storage and retrieval mechanisms of the data, especially in terms of scalability and accessibility.
+
+        Based on the above, provide a concise analysis that a financial analyst would find valuable.
+        """
+        holistic_analysis = get_gpt3_summary(holistic_analysis_prompt)
+        st.write(holistic_analysis)
 else:
     st.write("Please upload a CSV file.")
